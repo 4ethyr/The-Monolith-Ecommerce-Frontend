705333786d6b36dcc27ef59f3f87699c2376e46b
4ethyr <263508957+4ethyr@users.noreply.github.com>
2026-04-20 12:10:38 -0300

    feat: implement home page with component composition
    - Replace boilerplate content with Header, HeroBanner, and Footer
    - Improve page structure and maintainability

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import HeroBanner from "@/components/HeroBanner/HeroBanner";
+
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <HeroBanner/>
+     <Footer/>
+    </div>
+  );
+}
